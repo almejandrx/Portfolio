6715c265d4f08cda62f6ed52158d06bd2d5b2d06
almejandrx <alehrafar@gmai.com>
2023-11-22 22:40:05 -0400

    commit

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+setTimeout(function() {
+    document.getElementById('loading-screen').classList.add('hidden');
+    document.getElementById('content').classList.remove('hidden');
+    document.getElementById('content').style.opacity = 1;
+}, 1000); 
+
+var links = document.querySelectorAll('header nav ul li a');
+links.forEach(function(link) {
+  link.addEventListener('mouseenter', function() {
+    links.forEach(function(otherLink) {
+      if (otherLink !== link) {
+        otherLink.classList.add('opaque');
+      }
+    });
+  });
+
+  link.addEventListener('mouseleave', function() {
+    links.forEach(function(otherLink) {
+      otherLink.classList.remove('opaque');
+    });
+  });
+});
